d7bd31ebb34493c1b4ca3dd9393436662f30eae1
paulhorda <paulhorda02480@gmail.com>
2023-12-04 16:26:08 +0200

    Create Entity

---
+++ Homework-1/app/src/main/java/com/example/homework1/data/model/Project.kt
@@ -0,0 +1,11 @@
+package com.example.homework1.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "projects")
+data class Project(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String
+)
---
+++ Homework-1/app/src/main/java/com/example/homework1/data/model/Task.kt
@@ -0,0 +1,13 @@
+package com.example.homework1.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val projectId: Long,
+    var time: Double
+)
