f03bb33fb116da206be4ea1a5cdf452410e89409
paulhorda <paulhorda02480@gmail.com>
2023-12-13 17:15:37 +0200

    AN-2. Config MainActivity

---
+++ Homework2Network/app/src/main/java/com/example/homework2_network/ui/activity/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.homework2_network.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.homework2_network.ui.navigation.AppNavHost
+import com.example.homework2_network.ui.theme.Homework2NetworkTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Homework2NetworkTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavHost(navController = rememberNavController())
+                }
+            }
+        }
+    }
+}
