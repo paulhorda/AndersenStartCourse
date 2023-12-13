8f403e24478981db956c0a4726691eda45f7f868
paulhorda <paulhorda02480@gmail.com>
2023-12-13 17:13:02 +0200

    AN-2. Config gradle files

---
+++ Homework2Network/build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.49")
+    }
+}
+
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+}
