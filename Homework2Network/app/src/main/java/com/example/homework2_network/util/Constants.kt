3114c6ecf850f4fd2dcb3cc215a59e2ceaa6be7a
paulhorda <paulhorda02480@gmail.com>
2023-12-13 17:14:49 +0200

    AN-2. Setup Constants

---
+++ Homework2Network/app/src/main/java/com/example/homework2_network/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.homework2_network.util
+
+class Constants {
+    companion object {
+
+        const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        const val LISTS_ENDPOINT = "photos"
+        const val LIST_ID_ENDPOINT = "photos/{id}"
+    }
+
+    object Screens {
+        const val HOME_SCREEN = "home_screen"
+        const val DETAIL_SCREEN = "detail_screen"
+    }
+}
